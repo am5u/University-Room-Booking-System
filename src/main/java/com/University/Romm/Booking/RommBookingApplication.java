136c3daec55c388b673a7000342115b1ba3237f0
Ammar Mohamed <ammareldesouki130@gmail.com>
2025-04-22 18:02:48 +0200

    blank project

---
+++ src/main/java/com/University/Romm/Booking/RommBookingApplication.java
@@ -0,0 +1,13 @@
+package com.University.Romm.Booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RommBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RommBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/University/Romm/Booking/RommBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.University.Romm.Booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RommBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
