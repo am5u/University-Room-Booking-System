4f172d367def57794c2c29107930d1aae00241bc
Shahd <shahd.mostafa12@gmail.com>
2025-05-07 03:14:21 +0300

    Microservices

--- eureka-server/src/main/java/com/university/eureka_server/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/university/eureka_server/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.hospital.eureka_server;
+package com.university.eureka_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
